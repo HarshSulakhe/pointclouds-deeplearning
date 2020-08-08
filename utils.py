bb511d76a4807bd352909ef866fa0bd593200588
HarshSulakhe <sulakheharsh@gmail.com>
2020-08-09 04:12:42 +0530

    Add sampler class

--- utils.py
+++ utils.py
@@ -5,6 +5,56 @@ import math
 import random
 import os
 
+class PointSampler(object):
+"""
+Since original point clouds are not visually representative of the corresponding object, points are sampled randomly on each of the faces of the mesh.
+The following code has not been written by me, due credit goes to https://github.com/nikitakaraevv
+"""
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, int)
+        self.output_size = output_size
+
+    def triangle_area(self, pt1, pt2, pt3):
+        side_a = np.linalg.norm(pt1 - pt2)
+        side_b = np.linalg.norm(pt2 - pt3)
+        side_c = np.linalg.norm(pt3 - pt1)
+        s = 0.5 * ( side_a + side_b + side_c)
+        return max(s * (s - side_a) * (s - side_b) * (s - side_c), 0)**0.5
+
+    def sample_point(self, pt1, pt2, pt3):
+        # barycentric coordinates on a triangle
+        # https://mathworld.wolfram.com/BarycentricCoordinates.html
+        s, t = sorted([random.random(), random.random()])
+        f = lambda i: s * pt1[i] + (t-s)*pt2[i] + (1-t)*pt3[i]
+        return (f(0), f(1), f(2))
+
+
+    def __call__(self, mesh):
+        verts, faces = mesh
+        verts = np.array(verts)
+        areas = np.zeros((len(faces)))
+
+        for i in range(len(areas)):
+            areas[i] = (self.triangle_area(verts[faces[i][0]],
+                                           verts[faces[i][1]],
+                                           verts[faces[i][2]]))
+
+        sampled_faces = (random.choices(faces,
+                                      weights=areas,
+                                      cum_weights=None,
+                                      k=self.output_size))
+
+        sampled_points = np.zeros((self.output_size, 3))
+
+        for i in range(len(sampled_faces)):
+            sampled_points[i] = (self.sample_point(verts[sampled_faces[i][0]],
+                                                   verts[sampled_faces[i][1]],
+                                                   verts[sampled_faces[i][2]]))
+
+        return sampled_points
+
+
 def read_off(filepath):
     """
     The description of a .off file can be found at https://shape.cs.princeton.edu/benchmark/documentation/off_format.html.
@@ -101,7 +151,7 @@ def rotate_pointcloud(pointcloud):
     Explanation can be found at https://en.wikipedia.org/wiki/Rotation_matrix#Basic_rotations.
     pointcloud is of shape (N,3) where N is the number of vertices.
     """
-    angle = theta*2*math.pi*np.random.random()
+    theta = 2*math.pi*np.random.random()
     rotation_matrix = np.array([[math.cos(theta),-math.sin(theta),0],[math.sin(theta),math.cos(theta),0],[0,0,1]])
 
     rotated_pointcloud = np.dot(rotation_matrix,pointcloud.T).T ## Shape N,3

c12681109fba042c5e0a4139b92af77d0f9272a8
HarshSulakhe <sulakheharsh@gmail.com>
2020-08-10 03:53:48 +0530

    Upload loss

---
+++ pointnet/loss.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+class PointNetLoss(nn.Module):
+
+    def __init__(self,alpha = 0.0001    ):
+        super(PointNetLoss,self).__init__()
+        self.alpha = alpha
+
+    def forward(self,outputs,labels,m3,m64):
+        criterion = nn.NLLLoss()
+        bs = outputs.size()[0]
+        eye3 = torch.eye(3).repeat(bs,1,1)
+        eye64 = torch.eye(64).repeat(bs,1,1)
+        if torch.cuda.is_available():
+            eye3 = eye3.cuda()
+            eye64 = eye46.cuda()
+        diff64 = eye64 - torch.bmm(m64,m64.T)
+        diff3 = eye3 - torch.bmm(m3,m3.T)
+        return criterion(outputs,labels) + self.alpha*(torch.norm(diff3)+torch.norm(diff64)) / float(bs)

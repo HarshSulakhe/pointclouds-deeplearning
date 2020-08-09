d08474a96bdc8fa18f79621541f739f7a819dea4
HarshSulakhe <sulakheharsh@gmail.com>
2020-08-10 03:25:20 +0530

    Add TransformNet model

--- pointnet/model.py
+++ pointnet/model.py
@@ -35,3 +35,36 @@ class TNet(nn.Module):
             eye = eye.cuda()
         assert(eye.size()==x.size())
         return x + eye
+
+
+class TransformNet(nn.Module):
+
+    def __init__(self):
+        super(TransformNet,self).__init__()
+        self.T1 = TNet(3) #input transform
+        self.T2 = TNet(64) #feature transform
+        self.conv_1 = nn.Conv1d(3,64,1)
+        self.conv_2 = nn.Conv1d(64,128,1)
+        self.conv_3 = nn.Conv1d(128,1024,1)
+        self.bn_1 = nn.BatchNorm1d(64)
+        self.bn_2 = nn.BatchNorm1d(128)
+        self.bn_3 = nn.BatchNorm1d(1024)
+
+    def forward(self,x):
+        m3 = self.T1(x)
+        # x is of shape (bs,3,N)
+        x = x.transpose(1,2)
+        # x is now (bs,N,3)
+        # apply input transform
+        x = torch.bmm(x,m3).transpose(1,2)
+        x = F.relu(self.bn_1(self.conv_1(x)))
+        # x is now (bs,N,64)
+        # similarly for the feature space
+        m64 = self.T2(x)
+        x = x.transpose(1,2)
+        x = torch.bmm(x,m64).transpose(1,2)
+        x = F.relu(self.bn_2(self.conv_2(x)))
+        x = F.relu(self.conv_3(x))
+        x = nn.MaxPool1d(x.size()[-1])(x)
+        out = x.view(x.size()[0],-1)
+        return out,m3,m64
